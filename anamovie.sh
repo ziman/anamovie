ec190a9d4ed965f875a933d6510132afbaea4318
Matus Tejiscak <ziman@functor.sk>
2010-10-03 16:07:41 +0200

    yuvfilter accepts two pipes.

--- anamovie.sh
+++ anamovie.sh
@@ -3,11 +3,26 @@
 infile="$1"
 tmpdir="$2"
 
+inter_c() {
+	ffmpeg -i - -b 800k "$1"
+}
+
+inter_d() {
+	ffmpeg -i "$1" -f yuv4mpegpipe -
+}
+
+
 mkdir -p "$tmpdir"
 
 dims=$(ffmpeg -i "$1" -vstats 2>&1 | sed -rn 's/.*Stream.* ([0-9]+)x([0-9]+).*/\1:\2/p')
 w=${dims%:*}
 h=${dims#*:}
 w=$((w/2))
+x=0
 
-ffmpeg -i "$infile" -f yuv4mpegpipe -vf crop=0:0:$w:$h -
+for chan in left right; do
+	ffmpeg -i "$infile" -f yuv4mpegpipe -vf crop=$x:0:$w:$h - \
+		| ./yuvfilter $chan \
+		| inter_c "$tmpdir/$chan.mpg"
+	x=$((x+w))
+done
--- yuvfilter.c
+++ yuvfilter.c
@@ -5,11 +5,6 @@
 
 typedef unsigned char uchar;
 
-enum Mode {
-	mLeft,
-	mRight
-};
-
 static void die(const char * const msg)
 {
 	fprintf(stderr, "FATAL: %s\n", msg);
@@ -36,60 +31,51 @@ static inline uchar clamp(int v)
 	return (v < 0) ? 0 : ((v > 255) ? 255 : (uchar) v);
 }
 
-static inline void processLeftRGB(int rgb[3])
+static inline void processRGB(int left[3], int right[3], int res[3])
 {
-	rgb[0] = (rgb[1] + rgb[2]) / 2;
-	rgb[1] = 0;
-	rgb[2] = 0;
+	res[0] = (left[1] + left[2]) / 2;
+	res[1] = right[1];
+	res[2] = right[2];
 }
 
-static inline void processRightRGB(int rgb[3])
+#define y_2(p,q) ((2*y+(p))*width + (2*x+(q)))
+#define y(i) y_2(i >> 1, i & 1)
+void mogrifyYUVFrames(uchar * left, uchar * right, uchar * res, int width, int height)
 {
-	rgb[0] = 0;
-}
-
-#define y(p,q) Y[(2*y+p)*width + 2*x+q]
-void mogrifyYUVFrame(unsigned char * frame, int width, int height, enum Mode mode)
-{
-	uchar * Y = frame;
-	uchar * U = Y + width * height;
-	uchar * V = U + (width * height) / 4;
-	int ustride = width/2;
+	int uOfs = width * height;
+	int vOfs = uOfs / 4;
 	
-	for (int y = 0; y < height/2; ++y)
-		for (int x = 0; x < width/2; ++x)
+	uchar * LU = left + uOfs;
+	uchar * LV = LU + vOfs;
+	uchar * RU = right + uOfs;
+	uchar * RV = RU + vOfs;
+	uchar * OU = res + uOfs;
+	uchar * OV = OU + vOfs;
+
+	height /= 2;
+	width /= 2;
+	
+	for (int y = 0; y < height; ++y)
+		for (int x = 0; x < width; ++x)
 		{
+			int lrgb[4][3];
+			int rrgb[4][3];
 			int rgb[4][3];
 			int yuv[4][3];
-			int uvOfs = ustride*y + x;
-			int u = U[uvOfs];
-			int v = V[uvOfs];
-			yuv2rgb(rgb[0], y(0,0), u, v);
-			yuv2rgb(rgb[1], y(0,1), u, v);
-			yuv2rgb(rgb[2], y(1,0), u, v);
-			yuv2rgb(rgb[3], y(1,1), u, v);
-
-			if (mode == mLeft)
-				for (int i = 0; i < 4; ++i)
-				{
-					processLeftRGB(rgb[i]);
-					rgb2yuv(rgb[i], yuv[i]);
-				}
-			else
-				for (int i = 0; i < 4; ++i)
-				{
-					processRightRGB(rgb[i]);
-					rgb2yuv(rgb[i], yuv[i]);
-				}
-
+			for (int i = 0; i < 4; ++i)
+			{
+				yuv2rgb(lrgb[i], left[y(i)], *LU, *LV);
+				yuv2rgb(rrgb[i], right[y(i)], *RU, *RV);
+				processRGB(lrgb[i], rrgb[i], rgb[i]);
+				rgb2yuv(rgb[i], yuv[i]);
+				res[y(i)] = clamp(yuv[i][0]);
+			}
 		
-			y(0,0) = clamp(yuv[0][0]);
-			y(0,1) = clamp(yuv[1][0]);
-			y(1,0) = clamp(yuv[2][0]);
-			y(1,1) = clamp(yuv[3][0]);
-
-			U[uvOfs] = clamp((yuv[0][1] + yuv[1][1] + yuv[2][1] + yuv[3][1]) / 4);
-			V[uvOfs] = clamp((yuv[0][2] + yuv[1][2] + yuv[2][2] + yuv[3][2]) / 4);
+			*OU++ = clamp((yuv[0][1] + yuv[1][1] + yuv[2][1] + yuv[3][1]) / 4);
+			*OV++ = clamp((yuv[0][2] + yuv[1][2] + yuv[2][2] + yuv[3][2]) / 4);
+			++LU; ++RU;
+			++LV; ++RV;
+			
 		}
 }
 
@@ -101,54 +87,73 @@ void usage()
 	);
 }
 
-int main(int argc, char * argv[])
+FILE * sopen(const char * fn, char * format, int size)
 {
-	enum Mode mode = mLeft;
-	
-	if (argc != 2) usage();
-	if (!strcmp(argv[1], "left"))
-		mode = mLeft;
-	else if (!strcmp(argv[1], "right"))
-		mode = mRight;
-	else
-		usage();
+	FILE * f = fopen(fn, "rb");
+	if (!f) return f;
 	
 	// read YUV format
-	char format[2][1024];
-	for (int i = 0; i < 2; ++i)
-	{
-		fgets(format[i], 1024, stdin);
-		fprintf(stdout, "%s", format[i]);
-	}
+	fgets(format, size, f);
+	fprintf(stdout, "%s", format);
 
-	char * w = strstr(format[0], " W");
+	return f;
+}
+
+int getFrame(FILE * f, uchar * frame, int frameSize)
+{
+	char separator[1024];
+	fgets(separator, sizeof(separator), f);
+	if (feof(f)) return 1;
+	fprintf(stdout, "%s", separator);
+
+	fread(frame, frameSize, 1, f);
+	
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{
+	if (argc != 3) usage();
+
+	char format[1024];
+	
+	FILE * left  = sopen(argv[1], format, sizeof(format));
+	if (!left) die("Could not open left file.");
+	
+	FILE * right = sopen(argv[2], format, sizeof(format));
+	if (!right) die("Could not open right file.");
+	
+	char * w = strstr(format, " W");
 	if (!w) die("Could not determine image width.");
 	int width = atoi(w+2);
 
-	char * h = strstr(format[0], " H");
+	char * h = strstr(format, " H");
 	if (!h) die("Could not determine image height.");
 	int height = atoi(h+2);
 
-	char * c = strstr(format[0], " C");
+	char * c = strstr(format, " C");
 	if (!c) die("Could not determine pixel format.");
 	if (strncmp(c+2, "420", 3)) die("Only yuv420p pixel format supported!");
-
+	
 	int pixels = width * height;
 	int frameSize = pixels + pixels/2;
-	uchar * frame = (uchar *) malloc(frameSize);
-	while (!feof(stdin))
+	uchar * fLeft  = (uchar *) malloc(frameSize);
+	uchar * fRight = (uchar *) malloc(frameSize);
+	uchar * fResult= (uchar *) malloc(frameSize);
+	
+	while (1)
 	{
-		fread(frame, frameSize, 1, stdin);
-		if (feof(stdin)) break;
+		if (getFrame(left, fLeft, frameSize)) break;
+		if (getFrame(right, fRight, frameSize)) break;
 		
-		mogrifyYUVFrame(frame, width, height, mode);
-		fwrite(frame, frameSize, 1, stdout);
-
-		char separator[1024];
-		fgets(separator, sizeof(separator), stdin);
-		fprintf(stdout, "%s", separator);
+		mogrifyYUVFrames(fLeft, fRight, fResult, width, height);
+		
+		fwrite(fResult, frameSize, 1, stdout);
 	}
-	free(frame);
+	
+	free(fLeft);
+	free(fRight);
+	free(fResult);
 	
 	return 0;
 }
